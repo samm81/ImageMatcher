83225f798e2c61db027b1c0d94e55e64d69e385e
samm81 <samwmaynard@gmail.com>
2014-02-13 19:57:56 -0500

    Starting code Looks like it's not much to be proud of

---
+++ src/Runner.java
@@ -0,0 +1,63 @@
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import javax.imageio.ImageIO;
+
+
+public class Runner {
+
+	private final static int POLYGONS = 32;
+	private final static int SIDES = 3;
+	
+	public static void main(String args[]) throws InterruptedException, IOException{
+		BufferedImage pic = ImageIO.read(new File("pic.jpg"));
+		int width = pic.getWidth();
+		int height = pic.getHeight();
+
+		ImageBreeder breeder = new ImageBreeder(pic);
+
+		FaceMakerFrame fmf = new FaceMakerFrame(width, height);
+		FaceMakerFrame fmf2 = new FaceMakerFrame(width, height);
+		FaceMaker gene1 = new FaceMaker(fmf, POLYGONS, SIDES);
+		FaceMaker gene2 = new FaceMaker(gene1);		
+
+		int counter = 0;
+		int lc = 0;
+		while(true){
+			counter++;
+			
+			BufferedImage gene1Img = gene1.makeFace();
+			gene2.mutate();
+			BufferedImage gene2Img = gene2.makeFace();
+			
+			long score1 = breeder.getScore(gene1Img);
+			long score2 = breeder.getScore(gene2Img);
+			
+			//gene2.printSelf();
+			System.out.println(counter+"\t"+gene2.leeway+"\t"+score1+"\t("+score2+")");
+			
+			if(score2 < score1){
+				gene1 = new FaceMaker(gene2);
+				gene2.resetLeeway();
+				lc = 0;
+			}else{
+				gene2 = new FaceMaker(gene1);
+				lc++;
+			}
+			
+			if(lc == 10 * gene2.leeway){
+				gene2.increaseLeeway();
+				lc = 0;
+			}
+			
+			
+			fmf.updateFrame(gene1Img);
+			fmf.repaint();
+			fmf2.updateFrame(gene2Img);
+			fmf2.repaint();
+		}
+	}
+	
+}
